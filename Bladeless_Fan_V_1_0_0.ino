22bc2c5828c2185771d0f68d244b7caa74a4d9af
Elite Worm <60795660+EliteWorm@users.noreply.github.com>
2020-08-24 17:07:06 +0200

    Add files via upload

---
+++ Bladeless_Fan_V_1_0_0.ino
@@ -0,0 +1,24 @@
+//Bladeless Fan by Elite Worm (YouTube.com/c/EliteWorm)
+//Version 1.0.0
+
+//Connections:
+//Potentiometer wiper to P2
+//P0 to ESC input signal
+
+void setup() {
+  
+  pinMode(A1, INPUT);
+  pinMode(0, OUTPUT);
+  
+}
+
+void loop() {
+  
+  digitalWrite(0, HIGH);
+  int timehigh = map(analogRead(A1), 0, 1023, 1000, 1500);
+  delayMicroseconds(timehigh);
+  digitalWrite(0, LOW);
+  delayMicroseconds(10000);
+  delayMicroseconds(10000 - timehigh);
+
+}
